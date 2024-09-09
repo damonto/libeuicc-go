1bc1527ed38eda9d45102bea62348a9accc1fdc9
Damon To <damon.tu@outlook.com>
2024-09-09 15:13:20 +0800

    fix: improve example code

--- examples/main.go
+++ examples/main.go
@@ -27,9 +27,11 @@ func main() {
 		MatchingId: "123413231",
 	}, &libeuicc.DownloadOption{
 		ProgressBar: func(progress libeuicc.DownloadProgress) {
-			if progress == libeuicc.DownloadProgressAuthenticateServer {
+			// If you want to download, please remove this line.
+			if progress == libeuicc.DownloadProgressGetBoundProfile {
 				cancel()
 			}
+			fmt.Println(progress)
 		},
 		ConfirmationCodeFunc: func() string {
 			fmt.Println("Please input confirmation code:")
